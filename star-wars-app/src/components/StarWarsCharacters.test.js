7c90fca83b7ec022c05aed902118888c70edbb3d
Aaron Andersen <anderseana@tutanota.com>
2020-01-28 13:51:04 -0600

    merged the two button rendering tests into one.

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -5,11 +5,8 @@ import StarWarsCharacters from "./StarWarsCharacters";
 test('Checking to see if we are rendering the StarWarsCharacters component.', () => {
     render(<StarWarsCharacters/>);
 });
-test('Checking to see if the Next button is rendering.', () => {
+test('Checking to see if the Next and Previous buttons are rendering.', () => {
     const {queryByText} = render(<StarWarsCharacters/>);
     const nextButton = queryByText(/next/i);
-});
-test('Checking to see if we are rendering the Previous Button', () => {
-    const {queryByText} = render(<StarWarsCharacters/>);
     const previousButton = queryByText(/previous/i);
 });
